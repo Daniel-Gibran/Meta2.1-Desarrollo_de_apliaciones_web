95a8209a77e71afe403e43f5cd54ad475fba0399
Daniel Gibran <daniel.alvarez26@uabc.edu.mx>
2026-09-15 10:23:00 -0700

    Agregar indicador de carga a las imagenes

--- src/components/TarjetaConImagen.vue
+++ src/components/TarjetaConImagen.vue
@@ -1,6 +1,12 @@
 <template>
   <v-card height="100%">
-    <v-img :alt="title" cover height="220" :src="imageUrl" />
+    <v-img :alt="title" cover height="220" :src="imageUrl">
+      <template #placeholder>
+        <div class="d-flex align-center justify-center fill-height">
+          <v-progress-circular color="primary" indeterminate />
+        </div>
+      </template>
+    </v-img>
     <v-card-title>{{ title }}</v-card-title>
     <v-card-subtitle>Autor: {{ author }}</v-card-subtitle>
     <v-card-text>{{ description }}</v-card-text>
