1d55d3ab489ff48f72e65b894a252ca9c7ff6ff2
Daniel Gibran <daniel.alvarez26@uabc.edu.mx>
2026-09-15 10:28:20 -0700

    Actualizar datos del estudiante

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,10 +2,10 @@
   <v-footer class="mt-8" color="primary">
     <v-container>
       <v-row>
-        <v-col cols="12" md="3">Nombre: Estudiante Vue</v-col>
-        <v-col cols="12" md="3">Matrícula: 2026001</v-col>
+        <v-col cols="12" md="3">Nombre: Daniel Gibran Alvarez Armienta</v-col>
+        <v-col cols="12" md="3">Matrícula: 1183092</v-col>
         <v-col cols="12" md="3">Fecha: {{ date }}</v-col>
-        <v-col cols="12" md="3">Contacto: estudiante@ejemplo.com</v-col>
+        <v-col cols="12" md="3">Contacto: daniel.alvarez26@uacb.edu.mx</v-col>
       </v-row>
     </v-container>
   </v-footer>
